44db54b2a0f4a1fe6e567395955053ce0bf15379
Naimur <naimur.r61n@gmail.com>
2023-02-10 00:18:01 +0600

    Baner done

---
+++ pages/components/Banner.tsx
@@ -0,0 +1,55 @@
+import { Box, Button } from "@chakra-ui/react";
+import React from "react";
+import bg from "../../assets/home/Vector.png";
+import Rectangle from "../../assets/home/Rectangle 400.png";
+import Image from "next/image";
+
+const Banner = () => {
+	return (
+		// <Box bg="red.400">Box 3</Box>
+		<Box position="relative">
+			<Box
+				sx={{
+					// position: "absolute",
+					// zIndex: 1,
+					width: { sm: " 375px", lg: " 752px" },
+					height: { sm: "426px", lg: "849px" },
+					right: 0,
+					top: " 0px",
+				}}
+			>
+				<Image src={bg} alt="" />
+			</Box>
+			<Box
+				sx={{
+					position: "absolute",
+					zIndex: "-1",
+					width: { sm: " 378px", lg: " 735px" },
+					height: { sm: "413px", lg: "804px" },
+					right: 0,
+					top: "2px",
+				}}
+			>
+				<Image src={Rectangle} alt="" />
+			</Box>
+			<Button
+				colorScheme="white"
+				size="xs"
+				sx={{
+					position: "absolute",
+					zIndex: "3",
+					right: "20px",
+					top: "20px",
+					p: "15px",
+					border: "2px solid white",
+					rounded: 20,
+				}}
+			>
+				{" "}
+				Get In Touch
+			</Button>
+		</Box>
+	);
+};
+
+export default Banner;
--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,63 +1,15 @@
-import { Box, Button, Flex, Stack, Text, useMediaQuery } from "@chakra-ui/react";
+import { Box, Button, Stack } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
-import bg from "../../assets/home/Vector.png";
-import Rectangle from "../../assets/home/Rectangle 400.png";
+import Banner from "./Banner";
 
 const Header = () => {
-	const isNotSmallScreen = useMediaQuery("(min-width:600px");
-
 	return (
 		<>
-			<Flex justifyContent="space-between" className="header">
-				<Box bg="blue.400" zIndex={5}>
-					Box 3
-				</Box>
-				{/* <Box bg="red.400">Box 1</Box> */}
-
-				<Box position="relative" display="block">
-					<Box
-						sx={{
-							position: "absolute",
-							zIndex: 1,
-							width: { sm: " 375px", lg: " 752px" },
-							height: { sm: "426px", lg: "849px" },
-							right: 0,
-							top: " 0px",
-						}}
-					>
-						<Image src={bg} alt="" />
-					</Box>
-					<Box
-						sx={{
-							position: "absolute",
-							zIndex: "auto",
-							width: { sm: " 378px", lg: " 735px" },
-							height: { sm: "413px", lg: "804px" },
-							right: 0,
-							top: { sm: "-2px", lg: "2px" },
-						}}
-					>
-						<Image src={Rectangle} alt="" />
-					</Box>
-					<Button
-						colorScheme="white"
-						size="xs"
-						sx={{
-							position: "absolute",
-							zIndex: "3",
-							right: "20px",
-							top: "20px",
-							p: "15px",
-							border: "2px solid white",
-							rounded: 20,
-						}}
-					>
-						{" "}
-						Get In Touch
-					</Button>
-				</Box>
-			</Flex>
+			<Stack direction={["column-reverse", "row"]} justifyContent="space-between">
+				<Box bg="blue.400">Box 1</Box>
+				<Banner />
+			</Stack>
 		</>
 	);
 };
